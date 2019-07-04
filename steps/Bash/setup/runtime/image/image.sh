64e019651d4fdf7230c3bdd045cd06d5182022be
ankul <ankul@shippable.com>
2019-07-04 16:01:41 +0530

    pr comments

--- steps/Bash/setup/runtime/image/image.sh
+++ steps/Bash/setup/runtime/image/image.sh
@@ -11,9 +11,10 @@ boot_container() {
     exit $exit_code
   }
 
-  local pullCommand="docker pull $DOCKER_IMAGE"
   local dockerRegistry="%%context.registry%%"
-  if [ ! -z "$dockerRegistry" ]; then
+  local image_autopull="%%context.autoPull%%"
+  local pullCommand="docker pull $DOCKER_IMAGE"
+  configure_registry_credentials() {
     local intMasterName=$(eval echo "$"int_"$dockerRegistry"_masterName)
 
     if [ "$intMasterName" == "dockerRegistryLogin" ]; then
@@ -21,7 +22,7 @@ boot_container() {
       local password=$(eval echo "$"int_"$dockerRegistry"_password)
       local url=$(eval echo "$"int_"$dockerRegistry"_url)
 
-      retry_command docker login -u "$userName" -p "$password" "$url" &> /dev/null
+      retry_command docker login -u "$userName" -p "$password" "$url"
     elif [ "$intMasterName" == "amazonKeys" ]; then
       local accessKeyId=$(eval echo "$"int_"$dockerRegistry"_accessKeyId)
       local secretAccessKey=$(eval echo "$"int_"$dockerRegistry"_secretAccessKey)
@@ -34,7 +35,7 @@ boot_container() {
       aws configure set aws_secret_access_key "$secretAccessKey"
       aws configure set region "$region"
 
-      retry_command $(aws ecr get-login --no-include-email) &> /dev/null
+      retry_command $(aws ecr get-login --no-include-email)
     elif [ "$intMasterName" == "gcloudKey" ]; then
       local jsonKey=$(eval echo "$"int_"$dockerRegistry"_jsonKey)
       local projectId="$( echo "$jsonKey" | jq -r '.project_id' )"
@@ -43,44 +44,23 @@ boot_container() {
       local keyFile="$step_tmp_dir/$dockerRegistry/$dockerRegistry.json"
       touch $keyFile
       echo "$jsonKey" > $keyFile
-      gcloud -q auth activate-service-account --key-file "$keyFile" &> /dev/null
-      gcloud config set project "$projectId" &> /dev/null
-      gcloud docker -a &> /dev/null
+      gcloud -q auth activate-service-account --key-file "$keyFile"
+      gcloud config set project "$projectId"
+      gcloud docker -a
     elif [ "$intMasterName" == "artifactory" ]; then
       local url=$(eval echo "$"int_"$dockerRegistry"_url)
       local user=$(eval echo "$"int_"$dockerRegistry"_user)
       local apiKey=$(eval echo "$"int_"$dockerRegistry"_apikey)
       local sourceRepository="%%context.sourceRepository%%"
 
-      jfrog rt config --url "$url" --user "$user" --apikey "$apiKey" --interactive=false $dockerRegistry &> /dev/null
-      jfrog rt use $dockerRegistry &> /dev/null
+      jfrog rt config --url "$url" --user "$user" --apikey "$apiKey" --interactive=false $dockerRegistry
+      jfrog rt use $dockerRegistry
       pullCommand="jfrog rt docker-pull $DOCKER_IMAGE $sourceRepository"
     fi
-  fi
+  }
 
-  local image_autopull="%%context.autoPull%%"
-
-  if [ "$image_autopull" == "true" ]; then
-    start_group "Pulling Image"
-    execute_command "$pullCommand"
-    stop_group
-  fi
-
-  start_group "Booting Container"
-  local default_docker_options="-v /opt/docker/docker:/usr/bin/docker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $run_dir:$run_dir \
-    -v $pipeline_workspace_dir:$pipeline_workspace_dir \
-    -v $reqexec_dir:$reqexec_dir \
-    -w $(pwd) -d --init --rm --privileged --name $DOCKER_CONTAINER_NAME"
-  local docker_run_cmd="docker run $DOCKER_CONTAINER_OPTIONS $default_docker_options \
-    -e running_in_container=$running_in_container \
-    $DOCKER_IMAGE \
-    bash -c \"$reqexec_bin_path $steplet_script_path steplet.env\""
-
-  execute_command "$docker_run_cmd"
-
-  if [ ! -z "$dockerRegistry" ]; then
+  cleanup_registry_credentials() {
+    local intMasterName=$(eval echo "$"int_"$dockerRegistry"_masterName)
     if [ "$intMasterName" == "dockerRegistryLogin" ]; then
       local url=$(eval echo "$"int_"$dockerRegistry"_url)
       docker logout "$url"
@@ -113,8 +93,32 @@ boot_container() {
     elif [ "$intMasterName" == "artifactory" ]; then
       jfrog rt config delete $dockerRegistry --interactive=false
     fi
+  }
+
+  start_group "Booting Container"
+  if [ ! -z "$dockerRegistry" ]; then
+    execute_command configure_registry_credentials
+  fi
+  if [ "$image_autopull" == "true" ]; then
+    execute_command "$pullCommand"
   fi
 
+  local default_docker_options="-v /opt/docker/docker:/usr/bin/docker \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $run_dir:$run_dir \
+    -v $pipeline_workspace_dir:$pipeline_workspace_dir \
+    -v $reqexec_dir:$reqexec_dir \
+    -w $(pwd) -d --init --rm --privileged --name $DOCKER_CONTAINER_NAME"
+  local docker_run_cmd="docker run $DOCKER_CONTAINER_OPTIONS $default_docker_options \
+    -e running_in_container=$running_in_container \
+    $DOCKER_IMAGE \
+    bash -c \"$reqexec_bin_path $steplet_script_path steplet.env\""
+
+  execute_command "$docker_run_cmd"
+
+  if [ ! -z "$dockerRegistry" ]; then
+    execute_command cleanup_registry_credentials
+  fi
   stop_group
 
   wait_for_exit
